b672ce3c4a2449751341303381a68a6b9fd31cce
Dennis Martinez <dennis@dennmart.com>
2015-07-19 13:14:50 -0700

    Modified gulpfile to use gulp-server-livereload
    Taken from the Udemy course I'm taking.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,90 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
+var notifier = require('node-notifier');
+var server = require('gulp-server-livereload');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
 
-gulp.task('default', function() {
-  var bundler = watchify(browserify({
-    entries: ['./src/app.jsx'],
-    transform: [reactify],
-    extensions: ['.jsx'],
-    debug: true,
-    cache: {},
-    packageCache: {},
-    fullPaths: true
-  }));
+var notify = function(error) {
+  var message = 'In: ';
+  var title = 'Error: ';
 
-  function build(file) {
-    if (file) gutil.log("Recompiling " + file);
-    return bundler
-      .bundle()
-      .on('error', gutil.log.bind(gutil, 'Browserify error'))
-      .pipe(source('application.js'))
-      .pipe(buffer())
-      .pipe(uglify())
-      .pipe(gulp.dest('./assets'));
-  };
+  if(error.description) {
+    title += error.description;
+  } else if (error.message) {
+    title += error.message;
+  }
 
-  build();
+  if(error.filename) {
+    var file = error.filename.split('/');
+    message += file[file.length-1];
+  }
 
-  bundler.on('update', build);
+  if(error.lineNumber) {
+    message += '\nOn Line: ' + error.lineNumber;
+  }
+
+  notifier.notify({title: title, message: message});
+};
+
+var bundler = watchify(browserify({
+  entries: ['./src/app.jsx'],
+  transform: [reactify],
+  extensions: ['.jsx'],
+  debug: true,
+  cache: {},
+  packageCache: {},
+  fullPaths: true
+}));
+
+function bundle() {
+  return bundler
+    .bundle()
+    .on('error', notify)
+    .pipe(source('application.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets'))
+}
+bundler.on('update', bundle)
+
+gulp.task('build', function() {
+  bundle()
+});
+
+gulp.task('serve', function(done) {
+  gulp.src('')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        filter: function(filePath, cb) {
+          if(/main.js/.test(filePath)) {
+            cb(true)
+          } else if(/style.css/.test(filePath)){
+            cb(true)
+          }
+        }
+      },
+      open: true
+    }));
+});
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['build', 'serve', 'sass', 'watch']);
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
